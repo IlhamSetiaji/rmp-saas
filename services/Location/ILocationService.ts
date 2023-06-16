a7f1fada15fd47f1e4a28a3896a522346c87a406
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2023-06-16 10:50:53 +0700

    Modified location features

--- services/Location/ILocationService.ts
+++ services/Location/ILocationService.ts
@@ -1,12 +1,14 @@
 import { Location } from "@prisma/client";
+import { CreateLocationRequest } from "../../requests/Location/CreateLocationRequest";
 
 interface ILocationService {
     getLocations(): Promise<Location[]>;
-    createLocationByOrganization(organizationId: number, location: Location): Promise<Location>;
+    createLocationByOrganization(organizationId: number, location: CreateLocationRequest): Promise<Location>;
     getLocationsByOrganization(organizationId: number): Promise<Location[]>;
     getLocationById(locationId: number): Promise<Location>;
-    updateLocationById(locationId: number, location: Location): Promise<Location>;
+    updateLocationById(locationId: number, location: CreateLocationRequest): Promise<Location>;
     deleteLocationById(locationId: number): Promise<Location>;
+    employeeCheckLocation(organizationId: number, employeeId: number, locationId: number): Promise<any>;
 }
 
 export default ILocationService;
